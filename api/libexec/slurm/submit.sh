bf95afacdfeebc29743cd12d79c0f9f9f1ffdf7f
William McCumsite <william.mccumstie@gmail.com>
2021-02-16 18:50:39 +0000

    Report back the scheduler ID and cache it

--- api/libexec/slurm/submit.sh
+++ api/libexec/slurm/submit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sbatch "$1"
+# Submit the job to the scheduler
+output=$(sbatch "$1")
+if [[ $? -ne 0 ]]; then
+  exit $?
+fi
+
+echo "{ \"id\": \"$(echo "$output" | cut -d' ' -f4)\" }"
