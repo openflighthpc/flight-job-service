6db887f38491861b14ed122f4798fcfeaba5ec2c
Ben Armston <ben.armston@alces-software.com>
2021-02-22 19:25:00 +0000

    Fix URL for login api

--- client/src/account/CurrentUserContext.js
+++ client/src/account/CurrentUserContext.js
@@ -4,15 +4,24 @@ import useFetch from 'use-http';
 const initialState = null;
 const Context = React.createContext(initialState);
 
-function Provider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const { get, del, response } = useFetch(
-    process.env.REACT_APP_SSO_BASE_URL,
+const loginApiUrl = new URL(
+  process.env.REACT_APP_LOGIN_API_BASE_URL,
+  window.location.origin,
+).toString();
+
+export function useLoginApi() {
+  return useFetch(
+    loginApiUrl,
     {
       headers: { Accept: 'application/json' },
       credentials: 'include',
-    },
+    }
   );
+}
+
+function Provider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const { del, get, response } = useLoginApi();
 
   const actions = useMemo(
     () => ({
