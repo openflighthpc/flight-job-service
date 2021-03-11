5b85c29d70fa7d9ee729e1946528042bd785ca1a
Ben Armston <ben.armston@alces-software.com>
2021-03-11 16:18:39 +0000

    Better loading experience

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'flight-webapp-components/dist/styles/page-transitions.css'
 import 'flight-webapp-components/dist/styles/flight-webapp-components.css'
 import 'flight-webapp-components/dist/styles/fullscreen.css'
-import 'flight-webapp-components/dist/styles/fa-background.css'
+import 'flight-webapp-components/dist/styles/utils.css'
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
