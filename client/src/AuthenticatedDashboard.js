15c65a29a0a2a8a29557215b3cf7cfc7f6fdb3b7
Ben Armston <ben.armston@alces-software.com>
2021-03-18 17:36:44 +0000

    Include jobs page link on dashboard

--- client/src/AuthenticatedDashboard.js
+++ client/src/AuthenticatedDashboard.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import { DashboardLogo } from 'flight-webapp-components';
+import { DashboardLogo, useMediaGrouping } from 'flight-webapp-components';
 
 import { CardFooter } from './CardParts';
 
@@ -8,67 +8,139 @@ function AuthenticatedDashboard() {
   return (
     <div>
       <DashboardLogo />
-      <div className="card-deck">
-        <div className="card">
-          <div className="card-body fa-background fa-background-file-code-o">
-            <h5 className="card-title text-center">
-              Create job scripts from predefined templates
-            </h5>
-            <p className="card-text">
-              Create job scripts customized to your needs from predefined
-              templates.
-            </p>
-            <ol>
-              <li>
-                Answer a few simple questions.
-              </li>
-              <li>
-                Save the customized job script to your cluster.
-              </li>
-            </ol>
-          </div>
-          <CardFooter>
-            <Link
-              className="btn btn-success btn-block"
-              to="/templates"
-            >
-              <i className="fa fa-file-code-o mr-1"></i>
-              <span>Browse script templates</span>
-            </Link>
-          </CardFooter>
-        </div>
-
-        <div className="card">
-          <div className="card-body fa-background fa-background-file-text-o">
-            <h5 className="card-title text-center">
-              Submit your customized job script
-            </h5>
-            <p className="card-text">
-              Once you have created a customized job script, you can submit it
-              to be executed by your cluster.
-            </p>
-            <ol>
-              <li>
-                Select the script to submit.
-              </li>
-              <li>
-                Submit your script to the cluster's scheduler.
-              </li>
-            </ol>
-          </div>
-          <CardFooter>
-            <Link
-              className="btn btn-success btn-block"
-              to="/scripts"
-            >
-              <i className="fa fa-file-text-o mr-1"></i>
-              <span>Browse scripts</span>
-            </Link>
-          </CardFooter>
-        </div>
-      </div>
+      <Cards />
     </div>
   );
+}
+
+function Cards() {
+  const cards = [
+    (
+      <div className="card mb-2">
+        <div className="card-body fa-background fa-background-file-code-o">
+          <h5 className="card-title text-center">
+            Create job scripts from predefined templates
+          </h5>
+          <p className="card-text">
+            Create job scripts customized to your needs from predefined
+            templates.
+          </p>
+          <ol>
+            <li>
+              Answer a few simple questions.
+            </li>
+            <li>
+              Save the customized job script to your cluster.
+            </li>
+          </ol>
+        </div>
+        <CardFooter>
+          <Link
+            className="btn btn-success btn-block"
+            to="/templates"
+          >
+            <i className="fa fa-file-code-o mr-1"></i>
+            <span>Browse script templates</span>
+          </Link>
+        </CardFooter>
+      </div>
+    ),
+
+    (
+      <div className="card mb-2">
+        <div className="card-body fa-background fa-background-file-text-o">
+          <h5 className="card-title text-center">
+            Submit your customized job script
+          </h5>
+          <p className="card-text">
+            Once you have created a customized job script, you can submit it
+            to be executed by your cluster.
+          </p>
+          <ol>
+            <li>
+              Select the script to submit.
+            </li>
+            <li>
+              Submit your script to the cluster's scheduler.
+            </li>
+          </ol>
+        </div>
+        <CardFooter>
+          <Link
+            className="btn btn-success btn-block"
+            to="/scripts"
+          >
+            <i className="fa fa-file-text-o mr-1"></i>
+            <span>Browse scripts</span>
+          </Link>
+        </CardFooter>
+      </div>
+    ),
+
+    (
+      <div className="card mb-2">
+        <div className="card-body fa-background fa-background-database">
+          <h5 className="card-title text-center">
+            Monitor your jobs
+          </h5>
+          <p className="card-text">
+            Monitor your submitted jobs and view your results.
+          </p>
+        </div>
+        <CardFooter>
+          <Link
+            className="btn btn-success btn-block"
+            to="/scripts"
+          >
+            <i className="fa fa-database mr-1"></i>
+            <span>Monitor jobs</span>
+          </Link>
+        </CardFooter>
+      </div>
+    ),
+  ];
+
+  const { groupedItems } = useGrouping(cards);
+  const decks = groupedItems.map(
+    (group, index) => (
+      <div key={index} className="card-deck">
+        {
+          group.map((item, idx) => {
+            if (item == null) {
+              // The `key` attribute is intentionally omitted.
+              return <BlankCard />;
+            } else {
+              return item;
+            }
+          })
+        }
+      </div>
+    )
+  );
+  return decks;
+}
+
+function useGrouping(items) {
+  const { groupedItems, perGroup } = useMediaGrouping(
+    ['(min-width: 1200px)', '(min-width: 992px)', '(min-width: 768px)', '(min-width: 576px)'],
+    [2, 2, 1, 1],
+    1,
+    items,
+  );
+  if (groupedItems.length) {
+    const lastGroup = groupedItems[groupedItems.length - 1];
+    if (lastGroup.length < perGroup) {
+      const requiredBlanks = perGroup - lastGroup.length;
+      for (let i=0; i<requiredBlanks; i++) {
+        lastGroup.push(null);
+      }
+    }
+  }
+  return { groupedItems, perGroup };
+}
+
+function BlankCard() {
+  return <div className="card invisible"></div>;
 }
 
 export default AuthenticatedDashboard;
