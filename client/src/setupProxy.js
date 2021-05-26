51cf0aea87687da209514255d0f50c5183a27913
Ben Armston <ben.armston@alces-software.com>
2021-05-26 16:54:19 +0100

    Adjust default development config

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,28 +1,43 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  // Proxy requests to the api.
-  app.use(
-    '/job-scripts/api/v0',
-    createProxyMiddleware({
-      target: 'http://localhost:6310',
-      changeOrigin: false,
-      pathRewrite: {
-        '^/job-scripts/api/': '/', // Remove base path.
-      },
-      logLevel: 'debug',
-    })
-  );
+  if (process.env.REACT_APP_PROXY_API === "true") {
+    // Proxy requests to the desktop api.
+    const rewriteFrom = process.env.REACT_APP_PROXY_API_PATH_REWRITE_FROM ||
+      '^/dev/job-scripts/api';
+    const rewriteTo = process.env.REACT_APP_PROXY_API_PATH_REWRITE_TO ||
+      '/';
 
-  app.use(
-    '/login/api/v0',
-    createProxyMiddleware({
-      target: 'http://localhost:6311',
-      changeOrigin: false,
-      pathRewrite: {
-        '^/login/api/': '/', // Remove base path.
-      },
-      logLevel: 'debug',
-    })
-  );
+    app.use(
+      process.env.REACT_APP_PROXY_API_PATH || '/dev/job-scripts/api/v0',
+      createProxyMiddleware({
+        target: process.env.REACT_APP_PROXY_API_URL || 'http://localhost:6310',
+        changeOrigin: false,
+        pathRewrite: {
+          [rewriteFrom]: rewriteTo,
+        },
+        logLevel: 'debug',
+      })
+    );
+  }
+
+  if (process.env.REACT_APP_PROXY_LOGIN_API === "true") {
+    // Proxy requests to the login api.
+    const rewriteFrom = process.env.REACT_APP_PROXY_LOGIN_API_PATH_REWRITE_FROM ||
+      '^/dev/login/api';
+    const rewriteTo = process.env.REACT_APP_PROXY_LOGIN_API_PATH_REWRITE_TO ||
+      '/';
+
+    app.use(
+      process.env.REACT_APP_PROXY_LOGIN_API_PATH || '/dev/login/api/v0',
+      createProxyMiddleware({
+        target: process.env.REACT_APP_PROXY_LOGIN_API_URL || 'http://localhost:6311',
+        changeOrigin: false,
+        pathRewrite: {
+          [rewriteFrom]: rewriteTo,
+        },
+        logLevel: 'debug',
+      })
+    );
+  }
 };
