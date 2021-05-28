5018936267f9fc1b24b379f6945d754eea06e176
William McCumsite <william.mccumstie@gmail.com>
2021-05-28 15:47:44 +0100

    Prevent undefined requests on TemplatePage

--- client/src/TemplatePage.js
+++ client/src/TemplatePage.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { useRef } from 'react';
 
 import {
   DefaultErrorMessage,
@@ -11,10 +12,12 @@ import { useFetchTemplate } from './api';
 
 function TemplatePage() {
   const { id } = useParams();
-  const { data, error, loading } = useFetchTemplate(id);
+  const ref = useRef(id);
+
+  const { data, error, loading } = useFetchTemplate(ref.current);
 
   if (loading) {
-    return <Loading id={id} />;
+    return <Loading id={ref.current} />;
   } else if (error) {
     return <DefaultErrorMessage />;
   } else {
